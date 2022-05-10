2dec3362e536cfe66d2464c585860bc9164cf590
James Luty <luty0125@gmail.com>
2022-05-10 13:46:32 -0700

    initializing routing practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
+import Home from './Components/Home';
+import Four from './Components/Four';
+import HelloColored from './Components/HelloColored';
+import Hello from './Components/HelloColored';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        {" | "}
+        <Link to="/four">Four</Link>
+        {" | "}
+        <Link to="/hello">Hello</Link>
+        {" | "}
+        <Link to="/hello/blue/red">Hello with color</Link>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/four" element={<Four/>}/>
+          <Route path="/hello" element={<Hello/>}/>
+          <Route path="/hello/:text/:background" element={<HelloColored/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Four.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Four = () => {
+    return (
+        <div>
+            <h1>Four</h1>
+        </div>
+    )
+}
+
+export default Four
